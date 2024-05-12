b5621f2c1ab0183eb2e0b560657afe541ecae4cf
Ian Carlos Montoya Carmona <127963656+montoyaian@users.noreply.github.com>
2024-05-12 01:21:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 resource "null_resource" "run_ansible" {
   provisioner "local-exec" {
-    command = "sleep 20 && ansible-playbook ansi.yaml"
+    command = " ansible-playbook ansi.yaml"
   }
 }
 
