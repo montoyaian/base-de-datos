7a11e798cdb27b1d8aab0a9b60a61e6a6780ec74
Ian Carlos Montoya Carmona <127963656+montoyaian@users.noreply.github.com>
2024-05-12 00:42:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 
 
 resource "null_resource" "run_ansible" {
-  depends_on = [azurerm_sql_database.app_db]
-
   provisioner "local-exec" {
     command = "sleep 20 && ansible-playbook ansi.yaml"
   }
